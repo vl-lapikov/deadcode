a7832c9acb5e8fb4791798e4eef33b92ad562da1
Vladimir Lapikov <vl.lapikov@gmail.com>
2014-09-02 00:34:54 +0200

    example of deadcode.dump; analyze deadcode.dump and generate coverage;

--- analyze.php
+++ analyze.php
@@ -1,18 +1,115 @@
 <?php
 
+class Deadcode_AliveFile
+{
+    const INTERNAL = '___internal___';
+    const ALIVE = 'alive';
+    const START_LINE = 'startLine';
+    const END_LINE = 'endLine';
+    private $_classes = array();
+    private $_filename;
+
+    public function __construct($filename)
+    {
+        $this->_filename = $filename;
+        $this->parseFunctions();
+    }
+
+    public function getFilename()
+    {
+        return $this->_filename;
+    }
+
+    public function setAliveFunction($class, $name)
+    {
+        if ($class) {
+            $this->setClass($class);
+        } else {
+            $class = self::INTERNAL;
+        }
+        $this->_classes[$class][$name][self::ALIVE] = true;
+    }
+
+    public function getCodeCoverage()
+    {
+        $result = array();
+        foreach ($this->_classes as $class)
+        {
+            foreach ($class as $function) {
+                for ($i = $function[self::START_LINE]; $i <= $function[self::END_LINE]; $i++) {
+                    $result[$this->_filename][$i] = $function[self::ALIVE];
+                }
+            }
+        }
+        return $result;
+    }
+
+    private function setClass($class)
+    {
+        if (!isset($this->_classes[$class])) {
+            $this->_classes[$class] = array();
+        }
+        return $this;
+    }
+
+    private function parseFunctions()
+    {
+        $stream = new PHP_Token_Stream($this->_filename);
+        foreach ($stream->getFunctions() as $name => $function) {
+            $this->_classes[self::INTERNAL][$name] = array(
+                self::START_LINE => $function[self::START_LINE],
+                self::END_LINE => $function[self::END_LINE],
+                self::ALIVE => false,
+            );
+        }
+        foreach ($stream->getClasses() as $className => $class) {
+            foreach ($class['methods'] as $name => $function) {
+                $this->_classes[$className][$name] = array(
+                    self::START_LINE => $function[self::START_LINE],
+                    self::END_LINE => $function[self::END_LINE],
+                    self::ALIVE => false,
+                );
+            }
+        }
+    }
+}
+
+function getNextAliveFile($filename) {
+    static $handler, $passed;
+    if (!$handler) {
+        $handler = fopen($filename, 'r');
+    }
+    $newFile = false;
+    while (($line = fgets($handler)) !== false) {
+        $function = explode(':', $line);
+        if (!isset($function[0]) || !isset($function[2])) {
+            break;
+        }
+        $filename = trim($function[0]);
+        $class = trim($function[1]);
+        $function = trim($function[2]);
+        if (!$newFile && !isset($passed[$filename])) {
+            $newFile = new Deadcode_AliveFile($filename);
+        }
+        if ($newFile->getFilename() == $filename) {
+            $newFile->setAliveFunction($class, $function);
+        }
+    }
+    return $newFile;
+}
+
 require_once 'vendor/autoload.php';
 require_once 'Deadcode.php';
 
 $driver = new PHP_CodeCoverage_Driver_Deadcode(new PHP_CodeCoverage_Filter(), new PHP_CodeCoverage_Parser());
 
-$coverage = new PHP_CodeCoverage($driver);
-$coverage->start('example');
-$coverage->stop();
-
-$writer = new PHP_CodeCoverage_Report_HTML();
-$writer->process($coverage, '/tmp/coverage');
+while ($file = getNextAliveFile('deadcode.dump')) {
+    $coverage = new PHP_CodeCoverage($driver);
+    $coverage->append($file->getCodeCoverage(), $file->getFilename());
+    $writer = new PHP_CodeCoverage_Report_HTML();
+    $writer->process($coverage, '/tmp/coverage');
+}
 exit;
-
 $file = '/work/workspace/php/contract-module/src/application/controllers/ContractController.php';
 
 $function_happens = false;
--- example.php
+++ example.php
@@ -8,12 +8,26 @@ class ExampleClass
         echo "Example inner code" . PHP_EOL;
         return true;
     }
+
+    public function exampleDeadMethod()
+    {
+        echo __FUNCTION__ . PHP_EOL;
+        echo "Example dead inner code" . PHP_EOL;
+        return true;
+    }
 }
 
 function exampleFunction()
 {
     echo __FUNCTION__ . PHP_EOL;
     echo "Example inner code" . PHP_EOL;
+    return true;
+}
+
+function exampleDeadFunction()
+{
+    echo __FUNCTION__ . PHP_EOL;
+    echo "Example deadd inner code" . PHP_EOL;
     return true;
 }
 
